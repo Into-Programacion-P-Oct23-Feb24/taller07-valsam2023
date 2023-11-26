496c1f1f622257a4575e2cc981089911a1d1ef25
valsam <mvsamaniego1@utpl.edu.ec>
2023-11-26 15:33:57 -0500

    Subir cambios

--- problematicas/Problema05/src/problema05/Problema05.java
+++ problematicas/Problema05/src/problema05/Problema05.java
@@ -2,19 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package problema04;
+package problema05;
 
 /**
  *
  * @author Usuario
  */
-public class Problema04 {
+public class Problema05 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         int limite = 15;
         int numerador = 1;
         int denominador = 3;
@@ -22,7 +21,7 @@ public class Problema04 {
         String cadena = "1 ";
         int contador = 1;
 
-        while (denominador <= limite) {
+        do {
             if (contador % 2 == 0) {
                 signo = "+ ";
             } else {
@@ -33,7 +32,7 @@ public class Problema04 {
                     cadena, signo, numerador, denominador);
             denominador = denominador + 2;
             contador = contador + 1;
-        }
+        } while (denominador <= limite);
 
         System.out.println(cadena);
 
