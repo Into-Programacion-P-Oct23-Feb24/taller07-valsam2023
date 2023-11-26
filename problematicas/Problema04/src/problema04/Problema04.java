9e5361f1543ce9041538abe7c9c71b4a31538590
valsam <mvsamaniego1@utpl.edu.ec>
2023-11-26 15:12:41 -0500

    Subir cambios

---
+++ problematicas/Problema04/src/problema04/Problema04.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema04;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Problema04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int limite = 15;
+        int numerador = 1;
+        int denominador = 3;
+        String signo;
+        String cadena = "1 ";
+        int contador = 1;
+
+        do {
+            if (contador % 2 == 0) {
+                signo = "+";
+            } else {
+                signo = "-";
+            }
+
+            cadena = String.format("%s%s%d/%d ",
+                    cadena, signo, numerador, denominador);
+            denominador = denominador + 2;
+            contador = contador + 1;
+        } while (denominador <= limite);
+
+        System.out.println(cadena);
+
+    }
+}
